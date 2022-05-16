ab4746a16f407eabcf4c5d4a39f21ce774b3b147
Juan Pablo Carrillo Acero <carrilloacerojuanpablo@gmail.com>
2022-05-15 20:16:11 -0500

    Creación proyecto Spring Boot

---
+++ src/test/java/com/example/titanicinventory/TitanicInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.titanicinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
