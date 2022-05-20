95d18705dffff22577cbbaddf4d429ac07ebfbb5
Juan Pablo Carrillo Acero <carrilloacerojuanpablo@gmail.com>
2022-05-20 17:52:43 -0500

    get user info

--- src/main/java/com/example/titanicinventory/InicioController.java
+++ src/main/java/com/example/titanicinventory/InicioController.java
@@ -2,16 +2,25 @@ package com.example.titanicinventory;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class InicioController {
-
-    @RequestMapping("/inicio-sesion")
-    public String Inicio(@RequestParam(name="name", required=false, defaultValue="World")
-        String name, Model model){model.addAttribute("name", name);
+    @GetMapping("")
+    public String Inicio(){
+        System.out.println("INICIO!");
         return "inicio-sesion";
+    }
+
+    @RequestMapping("")
+    public String Inicio(@RequestParam("user") String user, @RequestParam("password") String password){
+        System.out.println("User: " + user);
+        System.out.println("Password: " + password);
+        return "redirect:home";
+    }
+
+    @RequestMapping("/home")
+    public String Home(){
+        return "home";
     }
 }
---
+++ src/main/java/com/example/titanicinventory/User.java
@@ -0,0 +1,18 @@
+package com.example.titanicinventory;
+
+public class User {
+    private String name;
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String s){
+        name = s;
+    }
+
+}
