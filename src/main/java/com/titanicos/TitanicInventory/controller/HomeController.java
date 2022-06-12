6bd9bcb310a720b8c5648d9391e55835802294c9
Juan Pablo Carrillo Acero <carrilloacerojuanpablo@gmail.com>
2022-06-12 00:50:36 -0500

    Agregar una gráfica

--- src/main/java/com/titanicos/TitanicInventory/controller/HomeController.java
+++ src/main/java/com/titanicos/TitanicInventory/controller/HomeController.java
@@ -6,8 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.util.HashMap;
+import java.util.Random;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HomeController {
+
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
 
     @RequestMapping("/admin")
     public String Home(@SessionAttribute(required=false,name="logged_user") User userAcc, final Model model){
@@ -17,10 +24,21 @@ public class HomeController {
             return "redirect:";
         }else if (userAcc.getRol().equals("administrador")) {
             model.addAttribute("logged_user", userAcc);
+            model.addAttribute("chartData", getChartData());
             return "admin";
         }else {
             System.out.println("Wrong role, redirecting...");
             return "redirect:";
         }
+    }
+
+    private List<List<Object>> getChartData() {
+        return List.of(
+                List.of("Mushrooms", RANDOM.nextInt(5)),
+                List.of("Onions", RANDOM.nextInt(5)),
+                List.of("Olives", RANDOM.nextInt(5)),
+                List.of("Zucchini", RANDOM.nextInt(5)),
+                List.of("Pepperoni", RANDOM.nextInt(5))
+        );
     }
 }
