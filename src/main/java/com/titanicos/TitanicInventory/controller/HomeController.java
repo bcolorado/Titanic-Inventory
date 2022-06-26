814f2947bb62896e787ed1cbf858a03982075a58
anfbalceroce <anfbalceroce@unal.edu.co>
2022-06-25 21:21:02 -0500

    se agrega opcion de mostrar estadisticas para un rango de fechas, cambio de total ingresos a long

--- src/main/java/com/titanicos/TitanicInventory/controller/HomeController.java
+++ src/main/java/com/titanicos/TitanicInventory/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.titanicos.TitanicInventory.controller;
 
+import com.titanicos.TitanicInventory.model.LogEvent;
 import com.titanicos.TitanicInventory.model.User;
 import com.titanicos.TitanicInventory.repositories.SaleRepository;
 import com.titanicos.TitanicInventory.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 import java.util.List;
 
@@ -21,21 +28,57 @@ public class HomeController {
     @Autowired
     SaleRepository saleRepo;
     @RequestMapping("/admin")
-    public String Home(@SessionAttribute(required=false,name="logged_user") User userAcc, final Model model){
+    public String Home(@RequestParam(name="from", required = false) String from, @RequestParam(name="to", required = false) String to, @SessionAttribute(required=false,name="logged_user") User userAcc, final Model model) throws ParseException {
         if (userAcc == null || userAcc.getRol() == null){
             return "redirect:";
         }else if (userAcc.getRol().equals("vendedor")) {
             return "redirect:";
         }else if (userAcc.getRol().equals("administrador")) {
-            System.out.println(userRepo.countByActiveAndRol(true,"vendedor"));
-            System.out.println(saleRepo.countByActive(true));
-            System.out.println(saleRepo.sumOfTotals());
-            System.out.println(saleRepo.productsSold());
-            System.out.println(saleRepo.productsIncome());
-            System.out.println(saleRepo.sellerSale());
-            System.out.println(saleRepo.sellerIngresos());
-            System.out.println(saleRepo.ingresosDia());
-            System.out.println(saleRepo.ventasDia());
+            if (from == null && to == null) {
+                System.out.println("# de vendedores: "+userRepo.countByActiveAndRol(true,"vendedor"));
+                System.out.println("# de administradores: "+userRepo.countByActiveAndRol(true,"administrador")); // cuenta a developer
+                System.out.println("# de usuarios activos: "+saleRepo.countActiveSellers());
+                System.out.println("# de ventas: "+saleRepo.countByActive(true));
+                System.out.println("# de ventas anuladas: "+saleRepo.countByActive(false));
+                System.out.println("Total ingresos: $"+saleRepo.sumOfTotals());
+                // listas de objetos "Queries", cada objeto tiene dos atributos, id y dato.
+                // Se imprime id dato
+                // id son las categorias para los de pie (productos y vendedores) o las bins para los de barras (dias)
+                // dato es el valor numerico
+                System.out.println(saleRepo.productsSold());
+                System.out.println(saleRepo.productsIncome());
+                System.out.println(saleRepo.sellerSale());
+                System.out.println(saleRepo.sellerIngresos());
+                System.out.println(saleRepo.ingresosDia());
+                System.out.println(saleRepo.ventasDia());
+                model.addAttribute("logged_user", userAcc);
+                model.addAttribute("chartData", getChartData());
+                return "admin";
+            }
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar c = Calendar.getInstance();
+            Date from_date = formatter.parse(from);
+            c.setTime(formatter.parse(to));
+            c.add(Calendar.DATE, 1);
+            Date to_date = c.getTime();
+
+            System.out.println("# de vendedores: "+userRepo.countByActiveAndRol(true,"vendedor"));
+            System.out.println("# de administradores: "+userRepo.countByActiveAndRol(true,"administrador")); // cuenta a developer
+            System.out.println("# de usuarios activos: "+saleRepo.countActiveSellersInRange(from_date, c.getTime()));
+            System.out.println("# de ventas: "+saleRepo.countByActiveInRange(true, from_date, c.getTime()));
+            System.out.println("# de ventas anuladas: "+saleRepo.countByActiveInRange(false, from_date, c.getTime()));
+            System.out.println("Total ingresos: $"+saleRepo.sumOfTotalsInRange(from_date, c.getTime()));
+            // listas de objetos "Queries", cada objeto tiene dos atributos, id y dato.
+            // Se imprime id dato
+            // id son las categorias para los de pie (productos y vendedores) o las bins para los de barras (dias)
+            // dato es el valor numerico
+            System.out.println(saleRepo.productsSoldInRange(from_date, c.getTime()));
+            System.out.println(saleRepo.productsIncomeInRange(from_date, c.getTime()));
+            System.out.println(saleRepo.sellerSaleInRange(from_date, c.getTime()));
+            System.out.println(saleRepo.sellerIngresosInRange(from_date, c.getTime()));
+            System.out.println(saleRepo.ingresosDiaInRange(from_date, c.getTime()));
+            System.out.println(saleRepo.ventasDiaInRange(from_date, c.getTime()));
+
             model.addAttribute("logged_user", userAcc);
             model.addAttribute("chartData", getChartData());
             return "admin";
--- src/main/java/com/titanicos/TitanicInventory/repositories/SaleRepository.java
+++ src/main/java/com/titanicos/TitanicInventory/repositories/SaleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface SaleRepository extends MongoRepository<Sales, String> {
@@ -13,10 +14,14 @@ public interface SaleRepository extends MongoRepository<Sales, String> {
     long countByActive(Boolean active);
     @Aggregation(pipeline ={ "{$match:{active:true}}",
             "{$group: { _id: '', total: {$sum: $total}}}"})
-    double sumOfTotals();
+    long sumOfTotals();
     @Query("{_id:'?0'}")
     Sales findSaleByID(String ID);
 
+    @Aggregation(pipeline = { "{$match:{active:true}}",
+            "{$group: { _id: '$id_vendedor', dato: {$sum:1}}}",
+            "{$count: 'id_vendedor'}"})
+    long countActiveSellers();
     @Aggregation(pipeline = { "{$match:{active:true}}",
             "{$unwind:{path:'$products'}}",
             "{$group: { _id: '$products._id', dato: {$sum: '$products.cantidad'}}}",
@@ -45,4 +50,43 @@ public interface SaleRepository extends MongoRepository<Sales, String> {
     List<Queries> ventasDia();
     @Query("{active:?0}")
     List<Sales> findSalesByActive(boolean active);
+
+
+    // mismas de arriba pero para fitlrar en rango
+    @Query(value = "{active:?0, 'timestamp' : {$gte : ?1, $lt : ?2}}",count = true)
+    long countByActiveInRange(Boolean active, Date from, Date to);
+    @Aggregation(pipeline ={ "{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$group: { _id: '', total: {$sum: $total}}}"})
+    long sumOfTotalsInRange(Date from, Date to);
+
+    @Aggregation(pipeline = { "{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$group: { _id: '$id_vendedor', dato: {$sum:1}}}",
+            "{$count: 'id_vendedor'}"})
+    long countActiveSellersInRange(Date from, Date to);
+    @Aggregation(pipeline = { "{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$unwind:{path:'$products'}}",
+            "{$group: { _id: '$products._id', dato: {$sum: '$products.cantidad'}}}",
+            "{$sort:{'dato':-1}}"})
+    List<Queries>productsSoldInRange(Date from, Date to);
+    @Aggregation(pipeline = { "{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$unwind:{path:'$products'}}",
+            "{$group: { _id: '$products._id', dato: {$sum: '$products.Subtotal'}}}",
+            "{$sort:{'dato':-1}}"})
+    List<Queries>productsIncomeInRange(Date from, Date to);
+    @Aggregation(pipeline = {"{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$group:{'_id':'$id_vendedor','dato':{$sum:1}}}",
+            "{$sort:{'dato':-1}}"})
+    List<Queries> sellerSaleInRange(Date from, Date to);
+    @Aggregation(pipeline = {"{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$group:{'_id':'$id_vendedor','dato':{$sum:$total}}}",
+            "{$sort:{'dato':-1}}"})
+    List<Queries> sellerIngresosInRange(Date from, Date to);
+    @Aggregation(pipeline = {"{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$group:{_id:{$dateToString:{format:'%Y-%m-%d', date:'$timestamp'}},'dato':{$sum:'$total'}}}",
+            "{$sort:{'_id':1}}"})
+    List<Queries> ingresosDiaInRange(Date from, Date to);
+    @Aggregation(pipeline = {"{$match:{active:true,timestamp : {$gte : ?0, $lt : ?1}}}",
+            "{$group:{_id:{$dateToString:{format:'%Y-%m-%d', date:'$timestamp'}},'dato':{$sum:1}}}",
+            "{$sort:{'_id':1}}"})
+    List<Queries> ventasDiaInRange(Date from, Date to);
 }
